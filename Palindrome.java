e2a4ce7ca41d98455a147cdef34db1b2e7633e8f
Your Name <you@example.com>
2024-01-20 20:27:15 +0200

    used scanner to read file

--- Palindrome.java
+++ Palindrome.java
@@ -1,3 +1,14 @@
+//author :Tendai K Nyevedzanai
+import java.io.*;
+import java.util.*;
 public class Palindrome {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("Enter the name of the file:");
+        String filename= scanner.nextLine();
+       
+    }
+    
+
     
 }
